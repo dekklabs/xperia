80ec1391d8b574aa6334ee3c16700dc569c1dd93
Jonathan <dekkservidores@gmail.com>
2024-02-02 10:22:17 -0500

    feat(ticket) WIP update ticket

--- src/controller/tickets/updateTicket.ts
+++ src/controller/tickets/updateTicket.ts
@@ -1,13 +1,24 @@
 import { Request, Response } from "express"
 import { errorResponse } from "../../helpers/errorResponse"
 import { responseData } from "../../helpers/responseData"
+import { Tickets } from "../../entities/Tickets"
+import { ticketRepository } from "../../repository/tickets"
 
 export const updateTicket = async (req: Request, res: Response) => {
   try {
+    const ticket = new Tickets()
     const { id } = req.params
-    const data = req.body
-    console.log(data)
-    console.log(id)
+    const { title, description } = req.body
+
+    ticket.title = title
+    ticket.description = description
+
+    await ticketRepository
+      .createQueryBuilder()
+      .update(ticket)
+      .set({ title, description })
+      .where("id = :id", {id})
+      .execute()
 
     responseData(res, "", "")
   } catch(error) {
